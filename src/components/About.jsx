52a954858dbb4f12ced2cc696dcf2956b91f4f14
Bharti277 <bhartideependra277@gmail.com>
2025-02-05 13:31:47 +0530

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,14 +22,10 @@ const About = () => {
         </p>
         <br />
         <p className="text-xl">
-          I am interested in being an Open Source Developer. I have 4+ years of
-          hands-on experience in web development and My motivation to learn
-          different technologies and algorithms drives me. I love the challenges
-          of a steep learning curve, which makes me a better learner. I strongly
-          desire to learn different technologies, which would enable me to
-          become a better developer. I believe in the fast execution culture
-          while building things from scratch. I believe in startups where the
-          learning process is very high and focuses on improving new ideas.
+          An ambitious, hard-working and punctual person seeking to work in a
+          good and challenging work environment where I can use my skills and
+          knowledge to do quality work under the guidance of best brains in the
+          country who are experts in their domain.
         </p>
       </div>
     </div>
