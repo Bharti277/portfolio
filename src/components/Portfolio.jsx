0994d843ff005ca51be65516aadcd538a157291a
Bharti277 <bhartideependra277@gmail.com>
2025-05-08 12:53:16 +0530

    wip

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,36 +1,49 @@
-import arrayDesctruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import { BrowserRouter, Link } from "react-router-dom";
+import weatherImg from "../assets/portfolio/weather.png";
+import flipEcommerceImg from "../assets/portfolio/flipEcommerce.png";
+import reactProjects from "../assets/portfolio/reactProjects.png";
+// import reactParallax from "../assets/portfolio/reactParallax.jpg";
+// import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+// import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDesctruct,
+      src: weatherImg,
+      live_link: "https://react-weather-app-live.vercel.app",
+      code: "https://github.com/Bharti277/weather-app",
     },
     {
       id: 2,
-      src: installNode,
+      src: flipEcommerceImg,
+      live_link: "https://redux-ecommerce-nu.vercel.app",
+      code: "https://github.com/Bharti277/redux-ecommerce",
     },
     {
       id: 3,
-      src: navbar,
+      src: reactProjects,
+      live_link: "https://react-styled-component-project.netlify.app",
+      code: "https://github.com/Bharti277/react-styled-component-project",
     },
-    {
-      id: 4,
-      src: reactParallax,
-    },
-    {
-      id: 5,
-      src: reactSmooth,
-    },
-    {
-      id: 6,
-      src: reactWeather,
-    },
+    // {
+    //   id: 4,
+    //   src: reactParallax,
+    //   live_link: "https://weather-app-eight-phi-99.vercel.app",
+    //   code: "https://github.com/Bharti277/weather-app",
+    // },
+    // {
+    //   id: 5,
+    //   src: reactSmooth,
+    //   live_link: "https://weather-app-eight-phi-99.vercel.app",
+    //   code: "https://github.com/Bharti277/weather-app",
+    // },
+    // {
+    //   id: 6,
+    //   src: reactWeather,
+    //   live_link: "https://weather-app-eight-phi-99.vercel.app",
+    //   code: "https://github.com/Bharti277/weather-app",
+    // },
   ];
   return (
     <div
@@ -46,7 +59,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid gap-8 px-12 sm:grid-cols-2 md:grid-cols-3 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, live_link, code }) => (
             <div key={id} className="rounded-lg shadow-md shadow-gray-600">
               <img
                 src={src}
@@ -54,12 +67,24 @@ const Portfolio = () => {
                 className="duration-200 rounded-md hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
-                </button>
+                <BrowserRouter>
+                  <Link
+                    to={live_link}
+                    className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                    target="_blank"
+                  >
+                    Demo
+                  </Link>
+                </BrowserRouter>
+                <BrowserRouter>
+                  <Link
+                    to={code}
+                    className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                    target="_blank"
+                  >
+                    Code
+                  </Link>
+                </BrowserRouter>
               </div>
             </div>
           ))}
